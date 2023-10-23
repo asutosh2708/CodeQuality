8cedaff54cd1cfcceec280d353ce3e6ad03e4b5d
asutosh2708 <55680953+asutosh2708@users.noreply.github.com>
2023-10-23 19:24:24 +0530

    Update Company.java

--- module2/codeReview/Company.java
+++ module2/codeReview/Company.java
@@ -1,18 +1,25 @@
-import java.io.*;
+   import java.io.*;
 
-public class Company { 
-    public static void main(String[] args){ 
-        System.out.println("First line");
-        System.out.println("Second line");
-        System.out.println("Third line");
-        int[] myIntArray = new int[]{1, 2, 3};
-        print4thItemInArray(myIntArray);
-        System.out.println("Fourth line");
-        System.out.println("Fith line");
-    } 
-    
-    private static void print4thItemInArray(int[] arr) {
-        System.out.println(arr[3]);
-        System.out.println("Fourth element successfully displayed!");
-    }
-}
+     public class Company {
+     
+         public static void main(String[] args) {
+     	// write your code here
+     	    System.out.println("First line");
+     	    System.out.println("Second line");
+     	    System.out.println("Third line");
+     
+     	    try {
+     	        int[] myIntArray = new int[]{1, 2, 3};
+     	        print4thItemInArray(myIntArray);
+     	    } catch (ArrayIndexOutOfBoundsException e){
+     	        System.out.println("The array doesn't have four items!");
+     	    }
+     
+     	    System.out.println("Fourth line");
+     	    System.out.println("Fith line");
+         }
+     
+         private static void print4thItemInArray(int[] arr) {
+             System.out.println(arr[3]);
+         }
+     }
