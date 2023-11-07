6c45a864a894bdd7631fc20df82d45e5d21f5edb
asutosh2708 <55680953+asutosh2708@users.noreply.github.com>
2023-11-08 01:23:18 +0530

    Update MobileDevice.java

--- module4/SOLID-main/dependency_inversion/good/src/MobileDevice.java
+++ module4/SOLID-main/dependency_inversion/good/src/MobileDevice.java
@@ -2,5 +2,8 @@
  * Created by mrk on 4/8/14.
  */
 public class MobileDevice implements Notifier {
-    
+    public void alertWeatherConditions(String weatherConditions) {
+    if (weatherConditions == "rainy")
+        System.out.print("It is rainy");
+    }
 }
