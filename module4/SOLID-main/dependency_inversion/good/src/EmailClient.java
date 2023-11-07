f374d976e90e28ce7645c50871d49ba36b67d348
asutosh2708 <55680953+asutosh2708@users.noreply.github.com>
2023-11-08 01:23:01 +0530

    Update EmailClient.java

--- module4/SOLID-main/dependency_inversion/good/src/EmailClient.java
+++ module4/SOLID-main/dependency_inversion/good/src/EmailClient.java
@@ -2,5 +2,8 @@
  * Created by mrk on 4/8/14.
  */
 public class EmailClient implements Notifier {
-    
+    public void alertWeatherConditions(String weatherConditions) {
+    if (weatherConditions == "sunny");
+        System.out.print("It is sunny");
+    }
 }
