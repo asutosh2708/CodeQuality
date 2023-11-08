f98a21bd7db2de732f10b9efb039815595c3ac42
asutosh2708 <55680953+asutosh2708@users.noreply.github.com>
2023-11-08 12:06:23 +0530

    Delete examples/solid-principles/src/com/github/samples/solid/lsp/violation/Plane.java

--- examples/solid-principles/src/com/github/samples/solid/lsp/violation/Plane.java
+++
@@ -1,9 +0,0 @@
-package com.github.samples.solid.lsp.violation;
-
-/**
- * Created on 16-04-2016.
- */
-public class Plane extends Vehicle {
-
-    // A plane can reverse engine gear while moving forward, no problem here
-}
