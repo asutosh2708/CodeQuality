929a27005dd9e33e3f1e9361c787fbc52d08a623
asutosh2708 <55680953+asutosh2708@users.noreply.github.com>
2023-11-08 12:11:53 +0530

    Add files via upload

---
+++ examples/solid-principles/src/com/github/samples/solid/lsp/violation/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    private int height;
+    private int width;
+
+    public void setHeight (int num) {
+        this.height = num;
+    }
+    public void setWidth (int num) {
+        this.width = num;
+    }
+}
---
+++ examples/solid-principles/src/com/github/samples/solid/lsp/violation/Square.java
@@ -0,0 +1,13 @@
+public class Square extends Rectangle {
+    private int height;
+    private int width;
+
+    public void setHeight (int num) {
+        this.height = num;
+        this.width = num;
+    }
+    public void setWidth (int num) {
+        this.width = num;
+        this.height = num;
+    }
+}
